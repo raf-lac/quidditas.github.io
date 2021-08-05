7ce7f44edde60a8660d79513cea88b0709a1de26
raf-lac <78952530+raf-lac@users.noreply.github.com>
2021-08-05 10:05:26 +0200

    nothing to say

---
+++ _site/main.js
@@ -0,0 +1,13 @@
+
+// STICKY HEADER
+/*
+stickybits('header', {
+    useStickyClasses: true
+});
+*/
+
+const headerHeight = document.querySelector('header').offsetHeight;
+
+stickybits('.signup', {
+    stickyBitStickyOffset: headerHeight
+})
